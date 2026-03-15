ec444692e560dd6d8e23369e69b60e5dcac1fe43
mcklepa <mwilk5@us.edu.pl>
2026-03-15 16:01:51 +0100

    basic calculator

--- calc/calc.php
+++ calc/calc.php
@@ -3,24 +3,34 @@
 $errors = array();
 $x = null;
 $y = null;
-
-function validate(){
-    
-}
+$wynik = null;
 
 // odebranie parametrów i walidacja
-if (!isset($_GET["x"]) && !isset($_GET["y"])){
- $errors[] = "nie podano parametrów";   
-}
+function validate(&$x,&$y,&$errors){
+    if (!isset($_GET["x"]) || !isset($_GET["y"])){
+        $errors[] = "nie podano parametrów";   
+    }
 
-$x = $_GET["x"];
-$y = $_GET["y"];
+    if(count($errors)) {return false;}
 
-if (!is_numeric($x)|| !is_numeric($y)){
-    $errors[] = "parametr nie jest liczbą";
+    $x = $_GET["x"];
+    $y = $_GET["y"];
+
+    if (!is_numeric($x) || !is_numeric($y)){
+        $errors[] = "parametr nie jest liczbą";
+    }
+    
+    $x = (float)$x;
+    $y = (float)$y;
+
+    if(count($errors)) {return false;}
+    return true;
 }
 
 // wykonanie akcji/dzialania
+if(validate($x,$y,$errors)) {
+    $wynik = $x * $y;
+}
 
 // wygenerowanie odpowiedzi
 include "calc_view.php";
--- calc/calc_view.php
+++ calc/calc_view.php
@@ -3,24 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
     <title>Calculator</title>
 </head>
 <body>
-    <form action="GET">
-        <label for="">wartość x: </label>
+    <form action="calc.php" method="GET" class="pure-form">
+        <label>wartość x: </label>
         <input type="text" name="x">
 
-        <label for="">wartość y: </label>
+        <label>wartość y: </label>
         <input type="text" name="y">
 
-        <input type="submit" value="oblicz">
+        <input type="submit" value="oblicz" class="pure-button pure-button-primary">
     </form>
 
+    <div style="padding: 10px; background-color: FF8888; border-radius: 5px;">
     <?php
     foreach($errors as $error){
-        echo $error;
-        echo <br>;
+        echo $error . "<br>";
     }
     ?>
+    </div>
+
+    <div style="padding: 10px; background-color: 88FF88; border-radius: 5px;">
+    <?php
+    if(isset($wynik)){
+        echo "Wynik: ".$wynik;
+    }
+    ?>
+    </div>
 </body>
 </html>
